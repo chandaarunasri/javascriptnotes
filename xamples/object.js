45815065e693465d97bc7ac70b6bf7c52925ee15
chandaarunasri <chandaarunasri.com>
2025-09-12 11:11:06 +0530

    object

---
+++ xamples/object.js
@@ -0,0 +1,18 @@
+ let a;
+        a ={
+            name :"BMW",
+            cost :7900000,
+            mileage :15,
+            color :"black" ,
+             start : function(){
+                console.log ("car is started");
+             },
+             stop : function(){
+                console.log("car is stopped");
+             },
+             Accelerate : function(){  
+                console.log("car is accelerated");
+             }
+
+        };
+        console.log(a.cost);
